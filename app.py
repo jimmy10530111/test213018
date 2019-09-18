0cf03db1630c7ef31820c8bb19dd4de4b71754bb
jimmy <55226372+jimmy10530111@users.noreply.github.com>
2019-09-18 22:58:08 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -37,7 +37,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    #message = TextSendMessage(text=event.message.text)
+    message="hello"
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
