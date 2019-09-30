903e998027a10fef58151bb9b7bc38d4ed3a6394
jimmy <55226372+jimmy10530111@users.noreply.github.com>
2019-09-30 15:12:43 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,15 +13,7 @@ from linebot.models import *
 
 app = Flask(__name__)
 
-connection = pymysql.connect(
-    host='36.232.130.64',
-    port=8080 ,
-    user='cat',
-    password='cat',
-    db='bug',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor)
-cursor = connection.cursor()
+
 
 
 
@@ -50,10 +42,8 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    cursor.execute("SELECT * FROM `test` WHERE `日期` LIKE '%s'"%(message))
-    result =message+ cursor.fetchall()
     line_bot_api.reply_message(event.reply_token, message)
-#    line_bot_api.reply_message(event.reply_token, result)
+    line_bot_api.push_message(U056904eae738c9778826ba74bc9f2d62, "test")
 
 import os
 if __name__ == "__main__":
