34ca4a4ef605edf7727944cfbf9e04fe64660ca7
jimmy <55226372+jimmy10530111@users.noreply.github.com>
2019-10-02 16:07:16 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -43,9 +43,11 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-    line_bot_api.push_message(U056904eae738c9778826ba74bc9f2d62, "test")
+    a201="test"
+    line_bot_api.push_message(U056904eae738c9778826ba74bc9f2d62,str(a201))
+    
 
-import os
+import 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
