acbe6279dd71037fe62efb747f18dbefa7b18a53
jimmy <55226372+jimmy10530111@users.noreply.github.com>
2019-10-03 18:56:25 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-    line.push_message("U056904eae738c9778826ba74bc9f2d62", "test")
+    test=str("test")
+    line.push_message("U056904eae738c9778826ba74bc9f2d62", test)
 
 import os
 if __name__ == "__main__":
