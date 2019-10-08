ae6639f41034d95dfa880faca81c9f2c2f967f73
jimmy <55226372+jimmy10530111@users.noreply.github.com>
2019-10-08 17:40:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -40,10 +40,12 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    a = "2019-10-07"
-    if event.message.txt == str(a):
-        message = TextSendMessage("測試")
-        line_bot_api.reply_message(event.reply_token, message)
+    #a = "2019-10-07"
+    #print(event.message.txt == str(a))
+    #if event.message.txt == str(a):
+        
+    message = TextSendMessage("測試")
+    line_bot_api.reply_message(event.reply_token, message)
     
 
 import os
